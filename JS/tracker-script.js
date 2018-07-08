016269ec633be70efb0ad60abb7791af483c3779
Abdul Bundu Kamara <bunduka2@gmail.com>
2018-07-07 22:50:49 -0400

    Adding second circle function and style
    Creating a seckond cirvle for the event tracker

--- JS/tracker-script.js
+++ JS/tracker-script.js
@@ -1,5 +1,6 @@
 const AREA = document.body;
 const CIRCLE = document.querySelector('.circle');
+const CIRCLE1 = document.querySelector('.circle1');
 
 var windowWidth = window.innerWidth;
 var windowHeight = window.innerHeight;
@@ -17,16 +18,38 @@ function mouseCoordinates(e) {
     CIRCLE.style.top = verticalPosition + 'px';
 }
 
+
+function mouseCoordinates1(e) {
+    // Capture the event object (mouse movement).
+    // Get X coordinate (distance from left viewport edge) via clientX property.
+    // Take total window width, subtract current coordinate and width of circle.
+    // Do the same for Y coordinate (distance from top viewport edge).
+    var horizontalPosition = windowWidth - e.clientX - 80;
+    var verticalPosition= windowHeight - e.clientY - 80;
+
+    // Set horizontal and vertical position.
+    CIRCLE1.style.left = horizontalPosition + 'px';
+    CIRCLE1.style.top = verticalPosition + 'px';
+}
+
 function changeColorOnTouch() {
     CIRCLE.style.backgroundColor = "green";
     CIRCLE.style.borderColor = "green";
 }
 
+function changeColorOnTouch1() {
+    CIRCLE1.style.backgroundColor = "blue";
+    CIRCLE1.style.borderColor = "blue";
+}
+
 // When the mouse moves, run the mouseCoordinates function.
 AREA.addEventListener('mousemove', mouseCoordinates, false);
+AREA.addEventListener('mousemove', mouseCoordinates1, false);
 
 // When the mouse touches the circle, run the changeColorOnTouch function.
 CIRCLE.addEventListener('mouseenter', changeColorOnTouch, false);
+CIRCLE1.addEventListener('mouseenter', changeColorOnTouch1, false);
 
 // When the mouse leaves the circle, remove inline styles with an anonymous function.
 CIRCLE.addEventListener('mouseleave', function(){CIRCLE.removeAttribute("style");}, false);
+CIRCLE1.addEventListener('mouseleave', function(){CIRCLE1.removeAttribute("style");}, false);
